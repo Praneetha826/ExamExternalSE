31a2a887ce23ddebb321f9ef7364801714265a88
Praneetha826 <palakurlapraneethagoud@gmail.com>
2025-12-02 13:43:10 +0530

    added maven java files

---
+++ src/main/java/demo/kmit/javaproject/App.java
@@ -0,0 +1,13 @@
+package demo.kmit.javaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
